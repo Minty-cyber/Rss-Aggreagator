a97ba17fc935b1ebdeef9d2bbfe0cada1c267252
Jmint <jeffreymintah737@gmail.com>
2024-11-13 08:51:56 +0000

    changes

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
+
+	// "github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -15,6 +20,21 @@ func main() {
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not available")
+	}
+
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+
+	}
+
+	log.Printf("Starting server on port %v", portString)
+	err := server.ListenAndServe()
+	if err !=nil {
+		log.Fatal(err)
+
 	}
 
 	fmt.Println("Port:", portString)
